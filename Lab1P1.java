f772c40ddcdeae25f9d2982e10611d25b0d34629
shashankreddy66 <88475927+shashankreddy66@users.noreply.github.com>
2025-09-03 11:29:38 +0530

    Add files via upload

---
+++ Lab1P1.java
@@ -0,0 +1,76 @@
+class Lab1P1{
+	public static void main(String...args){
+		byte b =15;
+		Byte bb = b;
+		//int x = 200;
+		//byte b =(byte)x;
+		
+		System.out.println(Byte.MAX_VALUE);
+		System.out.println(bb.MIN_VALUE);
+		System.out.println(Byte.SIZE);
+		System.out.println(Byte.TYPE);
+		System.out.println(Byte.BYTES);
+		
+		short s =15;
+		Short ss = s;
+		//int x = 200;
+		//byte b =(byte)x;
+		
+		System.out.println(Short.MAX_VALUE);
+		System.out.println(ss.MIN_VALUE);
+		System.out.println(Short.SIZE);
+		System.out.println(Short.TYPE);
+		System.out.println(Short.BYTES);
+		System.out.println("-----Short-----");
+		
+		
+		int i = 15;
+		Integer ii = i;
+		
+		System.out.println(Integer.MAX_VALUE);
+		System.out.println(Integer.MIN_VALUE);
+		System.out.println(Integer.SIZE);
+		System.out.println(Integer.TYPE);
+		System.out.println(Integer.BYTES);
+		System.out.println("----integ---");
+		
+		long l = 189;
+		Long ll =l;
+		
+		System.out.println(Long.MAX_VALUE);
+		System.out.println(ll.MIN_VALUE);
+		System.out.println(Long.SIZE);
+		System.out.println(Long.TYPE);
+		System.out.println(Long.BYTES);
+		System.out.println("----long---");
+		
+		float f = 128;
+		Float ff = f;
+		System.out.println(Float.MAX_VALUE);
+		System.out.println(ff.MIN_VALUE);
+		System.out.println(Float.SIZE);
+		System.out.println(Float.TYPE);
+		System.out.println(Float.BYTES);
+		System.out.println("----float---");
+		
+		double d = 128;
+		Double dd = d;
+		System.out.println(Double.MAX_VALUE);
+		System.out.println(dd.MIN_VALUE);
+		System.out.println(Double.SIZE);
+		System.out.println(Double.TYPE);
+		System.out.println(Double.BYTES);
+		System.out.println("----double---");
+		
+		boolean h = true;
+		Boolean hh = h;
+		System.out.println(Boolean.SIZE);
+		System.out.println(Boolean.TYPE);
+		System.out.println(Boolean.BYTES);
+		System.out.println("----boolean---");
+		
+		
+		
+		
+	} 
+}
