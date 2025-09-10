1c6cf6df5f3d353621e0e24d5c91ef64cb44b829
shashankreddy66 <88475927+shashankreddy66@users.noreply.github.com>
2025-09-10 11:17:44 +0530

    Add files via upload

--- LAB2/Lab2P1.java
+++ LAB2/Lab2P1.java
@@ -1,36 +1,17 @@
 import java.util.*;
-class Lab2P1{ 
-	public static void main(String...args){
+class Lab2P1
+{
+	public static void main(String...args)
+	{
+	
 		Scanner s = new Scanner(System.in);
-		System.out.println("enter the value");
-		int a = s.nextInt();
-		s.nextLine();
-		String op = s.nextLine();
-		
-		switch(op)
-		{ 
-			case "++":
-				System.out.println("Post:"+(a++));
-				System.out.println("Pre:"+(++a));
-			break;
-			case "--":
-				System.out.println("Post:"+(a++));
-				System.out.println("Pre:"+(++a));
-			break;
-			case "+":
-				a = a +a;
-				System.out.println("Unary:"+(a));
-			break;
-			case "-":
-				a = -a;
-				System.out.println("unary:"+(-a));
-			break;
-			case "~":
-				System.out.println("unary:"+(a));
-			break;
-			default:
-				System.out.println("hello");
-		
-	}
-	}
-	}
+		int x = s.nextInt();
+		int y = x;
+		System.out.println("output is:"+ x++ + ++x);
+		x = y;
+		System.out.println("output is:"+ (x++ + ++x));
+		x = y;
+		System.out.println(x++ + ++x +":+ output is:");
+		 
+	} 
+}
