0952182e8ea405df41ef9b0ce0e9900fd8751b05
shashankreddy66 <88475927+shashankreddy66@users.noreply.github.com>
2025-09-10 11:39:18 +0530

    Add files via upload

---
+++ LAB2/Lab2P3.java
@@ -0,0 +1,16 @@
+import java.util.*;
+class Lab2P3{
+	public static void main(String...args) { 
+		Scanner s = new Scanner(System.in);
+		int a =  s.nextInt();
+		int b =  s.nextInt();
+		String op = s.next();
+		String str = switch(op){ 
+			case "++" -> "Hello";
+			case "--" , "=", "-" -> "Not Good";
+			default -> "sorry";
+		};
+		System.out.println("Result is:"+str);				
+		
+	} 
+}
