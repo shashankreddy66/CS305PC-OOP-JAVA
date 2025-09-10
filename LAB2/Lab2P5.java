37bd301f6325ada28eaab916509b93c42b16846c
shashankreddy66 <88475927+shashankreddy66@users.noreply.github.com>
2025-09-10 12:34:32 +0530

    Add files via upload

---
+++ LAB2/Lab2P5.java
@@ -0,0 +1,22 @@
+import java.util.*;
+class Lab2P5{ 
+	public static void main(String...agrs){
+		Scanner s = new Scanner(System.in);
+		System.out.println("enter the value");
+		int a = s.nextInt();
+		int count = 0;
+		int i=2;
+		 prime;
+		while(i<0){
+		if(a%i==0)
+			prime = false;
+			i++;
+			}
+			if(prime){ 
+				System.out.println("its a prime number");
+			}
+			else{
+				System.out.println("its not a prime number");
+			}
+			}
+	}
